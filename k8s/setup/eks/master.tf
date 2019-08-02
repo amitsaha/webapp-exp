4afadbcf87cac529d15eb2d087725de0e0e8f40f
Amit Saha <amitsaha@users.noreply.github.com>
2019-08-02 16:00:26 +1000

    Update master.tf

--- k8s/setup/eks/master.tf
+++ k8s/setup/eks/master.tf
@@ -45,8 +45,8 @@ resource "aws_security_group" "cluster" {
 }
 
 resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
-  cidr_blocks       = ["10.0.56.0/21"] // vpn sydney
-  description       = "Allow workstation to communicate with the cluster API Server"
+  cidr_blocks       = "${var.ssh_access_ip}"
+  description       = "Allow local workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
   security_group_id = "${aws_security_group.cluster.id}"
@@ -74,39 +74,4 @@ resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
     "aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.cluster-AmazonEKSServicePolicy",
   ]
-}
-
-locals {
-  kubeconfig = <<KUBECONFIG
-
-
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.cluster.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.cluster.certificate_authority.0.data}
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
-      args:
-        - "token"
-        - "-i"
-        - "${var.cluster_name}"
-KUBECONFIG
-}
-
-output "kubeconfig" {
-  value = "${local.kubeconfig}"
 }
