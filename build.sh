0a4fdd7022508446d576aeb483660a3222831a3a
Amit Saha <amitsaha.in@gmail.com>
2019-08-28 11:27:52 +1000

    Nginx images

--- build.sh
+++ build.sh
@@ -2,16 +2,21 @@
 
 pushd golang/basic
 docker build -t amitsaha/webapp-demo:golang .
-docker push amitsaha/webapp-demo:golang
 popd
 
 
 pushd golang/tls
 docker build -t amitsaha/webapp-demo:golang-tls .
-docker push amitsaha/webapp-demo:golang-tls
 popd
 
 pushd python/django-gunicorn
 docker build -t amitsaha/webapp-demo:python-django .
-docker push amitsaha/webapp-demo:python-django
+popd
+
+pushd nginx/basic
+docker build -t amitsaha/nginx:basic
+popd
+
+pushd nginx/python-gunicorn
+docker build -t amitsaha/nginx:gunicorn
 popd
--- push.sh
+++ push.sh
@@ -4,3 +4,5 @@ set -e
 docker push amitsaha/webapp-demo:golang
 docker push amitsaha/webapp-demo:golang-tls
 docker push amitsaha/webapp-demo:python-django
+docker push amitsaha/nginx:basic
+docker push amitsaha/nginx:gunicorn
